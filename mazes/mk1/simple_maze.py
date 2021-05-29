0e11c611f3584aa9eb1feaf003f75369134cb2ba
harrywatkinson <harry@revmo.app>
2021-05-29 12:30:25 +0100

    added further tests

--- mazes/mk1/simple_maze.py
+++ mazes/mk1/simple_maze.py
@@ -1,5 +1,5 @@
 from random import randint
-from typing import List
+from typing import List, Tuple
 
 
 class SimpleMaze:
@@ -14,27 +14,65 @@ class SimpleMaze:
         'bottom':  '‾'
     }
 
-    def __init__(self, height: int = 5, width: int = 5):
+    def __init__(
+            self,
+            height: int = 5,
+            width: int = 5,
+    ):
 
-        self.maze = self.get_new_maze(height=height)
+        if height < 3:
+            raise ValueError('This maze is to short.')
 
-    def get_n_row(self, row_n: list, height: int) -> List[str]:
+        if width < 3:
+            raise ValueError('This maze is to narrow.')
+        self.height = height
+        self.width = width
+        self.maze = self.get_new_maze(
+            height=height,
+            width=width
+        )
 
-        clear_needed = height - 2
+    @staticmethod
+    def get_start_end_pos(width: int) -> Tuple[int, int]:
+
+        ub = width - 2
+
+        start_pos = randint(1, ub)
+
+        end_pos = randint(1, ub)
+
+        return start_pos, end_pos
+
+    def get_n_row(self, row_n: list, width: int = 5) -> List[str]:
+
+        clear_needed = width - 2
 
         row_n += [[self.markers['side']] + [self.markers['clear'] for _ in range(clear_needed)] + [self.markers['side']]]
 
         return row_n
 
-    def get_new_maze(self, height: int) -> List[List[str]]:
+    def get_new_maze(self, height: int, width: int) -> List[List[str]]:
 
         maze = []
-        maze += [[self.markers['top'], self.markers['top'],  self.markers['start'],  self.markers['top'], self.markers['top']]]
+        start_pos, finish_pos = self.get_start_end_pos(width=width)
+        top = [[self.markers['top'] for _ in range(width)]]
 
-        for _ in range(height):
-            self.get_n_row(maze, height)
+        top[0][start_pos] = self.markers['start']
 
-        maze += [[self.markers['bottom'], self.markers['bottom'],  self.markers['finish'], self.markers['bottom'], self.markers['bottom']]]
+        maze += top
+
+        # maze += [[self.markers['top'], self.markers['top'],  self.markers['start'],  self.markers['top'], self.markers['top']]]
+
+        height_needed = height - 2
+        for _ in range(height_needed):
+            self.get_n_row(maze, width)
+
+        # maze += [[self.markers['bottom'], self.markers['bottom'],  self.markers['finish'], self.markers['bottom'], self.markers['bottom']]]
+        bottom = [[self.markers['bottom'] for _ in range(width)]]
+
+        bottom[0][finish_pos] = self.markers['finish']
+
+        maze += bottom
 
         return maze
 
--- mazes/mk1/tests/test_maze.py
+++ mazes/mk1/tests/test_maze.py
@@ -1,38 +1,85 @@
+from pyte
+
 from mazes.mk1.simple_maze import SimpleMaze
 
 
 class TestMaze:
 
     test_cases = [
-        {'height': 5}
+        {
+            'height': 5,
+            'width': 5,
+            'exception': None
+        },
+        {
+            'height': 2,
+            'width': 2,
+            'exception': ValueError
+        },
+
     ]
 
     def test_start(self):
 
         for case in self.test_cases:
 
-            s = SimpleMaze(height=case['height'])
+            s = SimpleMaze(**case)
             maze = s.maze
+            width = case['width']
+            valid_pos = maze[0][1:width-1]
 
             start = 's'
 
-            assert maze[0][2] == start
+            assert start in valid_pos
 
-    def test_height(self):
+    def test_finish(self):
 
         for case in self.test_cases:
 
-            s = SimpleMaze(height=case['height'])
+            s = SimpleMaze(**case)
+            maze = s.maze
+            width = case['width']
+            height = case['height']
+            valid_pos = maze[height-1][1:width-1]
+
+            finish = 'f'
+
+            assert finish in valid_pos
+
+    def test_height_width(self):
+
+        for case in self.test_cases:
+
+            if case['exception']:
+
+
+            s = SimpleMaze(**case)
             maze = s.maze
 
+            assert len(maze) == case['height']
+
             for row in maze:
-                assert len(row) == case['height']
+                assert len(row) == case['width']
 
     def test_side(self):
 
         for case in self.test_cases:
 
-            s = SimpleMaze(height=case['height'])
+            s = SimpleMaze(**case)
             maze = s.maze
 
-            for row in maze:
+            height = case['height']
+            width = case['width']
+
+            void = {0, height-1}
+
+            for idx, row in enumerate(maze):
+
+                if idx not in void:
+
+                    assert row[0] == row[width-1] == '|'
+
+
+
+
+
