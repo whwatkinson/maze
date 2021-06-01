256a701a276a5f36ef54a6e8c33783a233668b8f
harrywatkinson <harry@revmo.app>
2021-06-01 19:30:33 +0100

    pointless commit

--- mazes/mk1/tests/test_wall.py
+++ mazes/mk1/tests/test_wall.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 from mazes.mk1 import SimpleWall
 
 
@@ -5,30 +7,26 @@ class TestWall:
 
     test_case = [
         {
-            'create_params':
-                {
-                    'height': 25,
-                    'width': 25,
-                    'number_of_walls': 25
-                },
-            'v': True,
-            'length': 5,
-            'x': 1,
-            'y': 3
+            'height': 25,
+            'width': 25,
+            'number_of_walls': 25
         }
     ]
 
     def test_get_wall_coords(self):
 
         for case in self.test_case:
-            v = case['v']
-            length = case['length']
-            x = case['x']
-            y = case['y']
+            w = SimpleWall(**case)
 
-            w = SimpleWall(**case['create_params'])
+            wall_meta = w.walls_meta
 
-            wall_coords = w.get_wall_coords(v=v, length=length, x=x, y=y)
+            for wall in wall_meta:
 
-            assert len(wall_coords) == case['length']
+                assert type(wall['v']) is bool
+                assert type(wall['length']) is int
+                assert type(wall['x']) is int
+                assert type(wall['y']) is int
+                assert type(wall['wall_coords']) is list
+
+            assert len(wall_meta) == case['number_of_walls']
 
