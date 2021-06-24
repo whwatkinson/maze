1d2e80768ae6c2cfbfbd571c3aaa8264e7b8d7c1
harrywatkinson <harry@revmo.app>
2021-06-24 20:39:30 +0100

    DHH

--- mazes/mk1/tests/test_simple_maze.py
+++ mazes/mk1/tests/test_simple_maze.py
@@ -59,17 +59,22 @@ class TestSimpleMaze:
             s = SimpleMaze(**case)
             maze = s.simple_maze
 
+            # Test height of maze
             assert len(maze) == case['height']
 
+            # Test width of maze
             for row in maze:
                 assert len(row) == case['width']
 
-    def test_side(self):
+    def test_markers(self):
 
         for case in self.test_cases:
 
             s = SimpleMaze(**case)
             maze = s.simple_maze
+
+            x_s, y_s = s.coords_start
+            x_f, y_f = s.coords_start
 
             height = case['height']
             width = case['width']
@@ -78,6 +83,20 @@ class TestSimpleMaze:
 
             for idx, row in enumerate(maze):
 
+                # Test side of maze
                 if idx not in void:
-
                     assert row[0] == row[width-1] == s.markers['side']
+
+                # Test top of the maze
+                if idx == 0:
+                    # Test start position
+                    if row == x_s and idx == y_s:
+                        assert row[y_s] == s.markers['start']
+                    # todo check rest DHH fix
+
+                # Test the bottom of the maze
+                if idx == height - 1:
+                    # Test finish position
+                    if row == x_f and idx == y_f:
+                        assert row[y_s] == s.markers['finish']
+                    # todo check rest DHH fix
